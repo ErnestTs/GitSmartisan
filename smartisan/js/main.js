43ea40850b0d9bb8b5dfc1e2a5284ae31a9935db
BowenHero <chushunshunlove@163.com>
2018-04-04 14:40:30 +0800

    first submit

---
+++ smartisan/js/main.js
@@ -0,0 +1,47 @@
+console.log("载入成功");
+
+/*
+    设置需要引入的js文件
+*/
+require.config({
+    paths: {
+        // 模块名字: 模块路径
+        "jquery": "jquery-1.11.3", //遵从AMD规范
+        "jquery-cookie": "jquery.cookie"
+    },
+    shim: {
+        /*
+            在实例的app中，还用到jquery以外的第三方库
+            如果该类库不是一个标准AMD规范，我又不想去改代码
+            需要通过下述方式定义该文件
+        */
+
+        //设置依赖关系
+        "jquery-cookie": ["jquery"]
+    }
+})
+
+
+
+//要去调用index.js中的main
+
+require(["register"],function(register){
+    console.log(register.register());
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
